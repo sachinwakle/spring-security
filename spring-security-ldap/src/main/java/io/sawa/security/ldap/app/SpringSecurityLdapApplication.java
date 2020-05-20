abad480d76389eb5471715909d97927d82611bd9
sachinwakle <sbwakle@hotmail.com>
2020-05-21 00:11:18 +0530

    Implemented spring security authentication with LDAP

---
+++ spring-security-ldap/src/main/java/io/sawa/security/ldap/app/SpringSecurityLdapApplication.java
@@ -0,0 +1,13 @@
+package io.sawa.security.ldap.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityLdapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityLdapApplication.class, args);
+	}
+
+}
---
+++ spring-security-ldap/src/test/java/io/sawa/security/ldap/app/SpringSecurityLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package io.sawa.security.ldap.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
